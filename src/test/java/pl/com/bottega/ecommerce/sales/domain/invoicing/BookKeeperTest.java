920860fd031d3fbd003413d257ad31ddc7552f6a
Michal <michalogloza97@gmail.com>
2020-04-28 00:41:09 +0200

    Defined test cases

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -0,0 +1,60 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+
+class BookKeeperTest {
+
+    @BeforeAll
+    static void init()
+    {
+
+    }
+
+    @BeforeEach
+    void fixture()
+    {
+
+    }
+
+    @Test
+    void emptyInvoiceTest()
+    {
+
+    }
+
+    @Test
+    void oneItemInvoiceTest()
+    {
+
+    }
+
+    @Test
+    void invoiceRequestArgumentNullTest()
+    {
+
+    }
+
+    @Test
+    void taxPolicyArgumentNullTest()
+    {
+
+    }
+
+    @Test
+    void calculateTaxNumberOfCallsTest()
+    {
+
+    }
+}
