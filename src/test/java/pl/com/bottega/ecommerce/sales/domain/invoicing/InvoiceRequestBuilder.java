2dd2742f394cad441b1496043bb8b5ffba52d476
Asmei <kamsykula@gmail.com>
2020-04-28 17:10:56 +0200

    builder implemented

---
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/InvoiceRequestBuilder.java
@@ -0,0 +1,36 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InvoiceRequestBuilder {
+	private static final InvoiceRequestBuilder INSTANCE = new InvoiceRequestBuilder();
+	private ClientData clientData = null;
+	private List<RequestItem> items = new ArrayList<>();
+	
+	private InvoiceRequestBuilder(){}
+	
+	public static InvoiceRequestBuilder getInstance(){
+		return INSTANCE;
+	}
+	
+	public void addItem(RequestItem item){
+		this.items.add(item);
+	}
+	
+	public void setClientData(ClientData clientData){
+		this.clientData = clientData;
+	}
+	
+	public InvoiceRequest build(){
+		if(this.clientData == null) this.clientData = Mockito.mock(ClientData.class);
+		
+		InvoiceRequest rV = new InvoiceRequest(this.clientData);
+		items.forEach(rV::add);
+		return rV;
+	}
+}
