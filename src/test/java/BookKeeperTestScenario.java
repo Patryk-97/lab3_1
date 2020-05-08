02c2fd79eac254b55c8edc264577fb0491244450
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-08 14:21:34 +0200

    Added base test scenario with two test cases declarations

---
+++ src/test/java/BookKeeperTestScenario.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+public class BookKeeperTestScenario {
+    @Test
+    void invoiceRequestWithTwoRequestShouldReturnInvoiceWithTwoItems(){
+
+    }
+
+    @Test
+    void invoiceRequestWithOneRequestShouldReturnInvoiceWithOneItem(){
+
+    }
+    
+}
