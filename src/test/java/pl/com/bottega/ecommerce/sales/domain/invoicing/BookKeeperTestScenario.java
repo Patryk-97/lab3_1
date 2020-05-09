53fb0fc7328c3f151d2d420612430562fc08659c
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-05-09 14:41:54 +0200

    Moved initialization of common test artifacts into annotated method init(), renamed  invoiceRequestWithTwoRequestShouldShouldCallCalculateTaxMethodTwice into invoiceRequestWithTwoItemsShouldCallCalculateTaxMethodTwice, I had to be blind or exhausted, to write incorrect name of method twice, implemented test case invoiceRequestWithTwoItemsShouldCallCalculateTaxMethodTwice.

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTestScenario.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTestScenario.java
@@ -1,4 +1,6 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
@@ -9,26 +11,53 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 
 class BookKeeperTestScenario {
 
-    @Test
-    void invoiceRequestWithTwoRequestShouldShouldCallCalculateTaxMethodTwice(){
+    private Tax testTax;
+    private TaxPolicy mockedPolicy;
+    private InvoiceFactory mockedFactory;
+    private ClientData mockedClientData;
 
+    @BeforeEach
+    void init() {
+        testTax = new Tax(Money.ZERO, "Test tax");
+        mockedPolicy = Mockito.mock(TaxPolicy.class);
+        mockedFactory = Mockito.mock(InvoiceFactory.class);
+        mockedClientData = Mockito.mock(ClientData.class);
     }
 
     @Test
-    void invoiceRequestWithOneRequestShouldReturnInvoiceWithOneItem(){
-        Tax testTax = new Tax(Money.ZERO, "");
-        TaxPolicy mockedPolicy = Mockito.mock(TaxPolicy.class);
+    void invoiceRequestWithTwoItemsShouldCallCalculateTaxMethodTwice() {
+        Mockito.doReturn(testTax)
+                .when(mockedPolicy)
+                .calculateTax(Mockito.any(ProductType.class), Mockito.any(Money.class));
 
+        Product testProduct1 = new Product(Id.generate(), Money.ZERO, "ProductMock1", ProductType.STANDARD);
+        Product testProduct2 = new Product(Id.generate(), Money.ZERO, "ProductMock2", ProductType.STANDARD);
+
+        Mockito.doReturn(new Invoice(Id.generate(), mockedClientData))
+                .when(mockedFactory)
+                .create(Mockito.any(ClientData.class));
+        InvoiceRequest request = new InvoiceRequest(mockedClientData);
+        request.add(new RequestItem(testProduct1.generateSnapshot(), 1, Money.ZERO));
+        request.add(new RequestItem(testProduct2.generateSnapshot(), 1, Money.ZERO));
+        BookKeeper keeper = new BookKeeper(mockedFactory);
+
+        Invoice result = keeper.issuance(request, mockedPolicy);
+        
+        assertThat(result, notNullValue());
+        Mockito.verify(mockedPolicy,Mockito.times(2)).calculateTax(Mockito.any(ProductType.class), Mockito.any(Money.class));
+    }
+
+    @Test
+    void invoiceRequestWithOneRequestShouldReturnInvoiceWithOneItem() {
         Mockito.doReturn(testTax)
                 .when(mockedPolicy)
                 .calculateTax(Mockito.any(ProductType.class), Mockito.any(Money.class));
 
         Product testProduct = new Product(Id.generate(), Money.ZERO, "ProductMock", ProductType.STANDARD);
-        InvoiceFactory mockedFactory = Mockito.mock(InvoiceFactory.class);
-        ClientData mockedClientData = Mockito.mock(ClientData.class);
 
         Mockito.doReturn(new Invoice(Id.generate(), mockedClientData))
                 .when(mockedFactory)
